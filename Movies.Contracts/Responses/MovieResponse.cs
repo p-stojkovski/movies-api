f41303908e80a60d1e26cbae05b0a0bd8d75e475
Petar Stojkovski <petars089@gmail.com>
2024-06-30 17:43:00 +0200

    Set up Ratings endpoints and update movie response to include total rating and user rating

--- Movies.Contracts/Responses/MovieResponse.cs
+++ Movies.Contracts/Responses/MovieResponse.cs
@@ -5,6 +5,8 @@ public class MovieResponse
     public required Guid Id { get; init; }
     public required string Title { get; init; }
     public required string Slug { get; init; }
+    public float? Rating { get; init; }
+    public int? UserRating { get; init; }
     public required int YearOfRelease { get; init; }
     public required IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
 }
