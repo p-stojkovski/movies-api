cc8c15dd7130d889f96ad6fbf473eb0fbb0ebbf4
Petar Stojkovski <petars089@gmail.com>
2024-06-30 17:55:39 +0200

    Added create table script for creating ratings table in DbInitializer

--- Movies.Application/Database/DbInitializer.cs
+++ Movies.Application/Database/DbInitializer.cs
@@ -34,5 +34,13 @@ public class DbInitializer
             movieId UUID references movies (Id),
             name TEXT not null);
          """);
+
+        await connection.ExecuteAsync("""
+            create table if not exists ratings (
+            userid UUID,
+            movieId UUID references movies (Id),
+            rating integer not null,
+            primary key (userid, movieid));
+         """);
     }
 }
